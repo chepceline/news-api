faeeac891c4de21d7a6fa03fec1fefe8e4db5f50
chepceline <chepceline25@gmail.com>
2022-05-03 11:43:34 +0300

    initial

--- app/models.py
+++ app/models.py
@@ -1,45 +1,26 @@
-class Article:
+class Sources:
     '''
-    Class that instantiates objects of the news article objects of the news sources
+    Sources class to define Sources Objects
     '''
-    def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
 
-class Category:
-    '''
-    Class that instantiates objects of the news categories objects of the news sources
-    '''
-    def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
-
-class Source:
-    '''
-    Source class to define source objects
-    '''
-    def __init__(self,id,name,description,url):
-        self.id = id
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
         self.name = name
         self.description = description
         self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
 
-class Headlines:
+class Articles:
     '''
-    Class that instantiates objects of the headlines categories objects of the news sources
+    Articles class to define articles objects
     '''
-    def __init__(self,author,description,time,url,image,title):
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
         self.author = author
+        self.title = title
         self.description = description
-        self.time = time
         self.url = url
         self.image = image
-        self.title = title
+        self.date = date
